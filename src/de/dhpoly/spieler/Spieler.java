bae509068f0dc227678f971a8fe22f0dab9a9077
K3 <K3@R-K-07>
2017-10-25 11:32:21 +0200

    Spieler neu, Player veraltet. Ab sofort Spieler verwenden!!!

--- src/de/dhpoly/spieler/Spieler.java
+++ src/de/dhpoly/spieler/Spieler.java
@@ -1,5 +1,22 @@
 package de.dhpoly.spieler;
-public interface Spieler 
-{
 
+import de.dhpoly.player.Geldhaber;
+
+public interface Spieler
+{
+	public int getBargeld();
+
+	public void einzahlen(int betrag);
+
+	public void auszahlen(int betrag);
+
+	public void ueberweiseGeld(int betrag, Geldhaber empfaenger);
+
+	public boolean isNegative();
+
+	public int getFeldNr();
+
+	public String getName();
+
+	public void setFeldNr(int feldNrSoll);
 }
--- src/de/dhpoly/spieler/control/SpielerImpl.java
+++ src/de/dhpoly/spieler/control/SpielerImpl.java
@@ -1,6 +1,59 @@
 package de.dhpoly.spieler.control;
+
+import de.dhpoly.player.Geldhaber;
 import de.dhpoly.spieler.Spieler;
+import de.dhpoly.spieler.model.GeldhaberImpl;
+
 public class SpielerImpl implements Spieler
 {
+	private int feldNr = 0;
+	private String name;
+	private int bargeld;
 
+	public SpielerImpl(String name, int startguthaben)
+	{
+		this.name = name;
+		bargeld = startguthaben;
+	}
+
+	public int getFeldNr()
+	{
+		return feldNr;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setFeldNr(int feldNrSoll)
+	{
+		this.feldNr = feldNrSoll;
+	}
+
+	public int getBargeld()
+	{
+		return bargeld;
+	}
+
+	public void einzahlen(int betrag)
+	{
+		bargeld += betrag;
+	}
+
+	public void auszahlen(int betrag)
+	{
+		bargeld -= betrag;
+	}
+
+	public void ueberweiseGeld(int betrag, Geldhaber empfaenger)
+	{
+		empfaenger.einzahlen(betrag);
+		this.auszahlen(betrag);
+	}
+
+	public boolean isNegative()
+	{
+		return bargeld >= 0;
+	}
 }
