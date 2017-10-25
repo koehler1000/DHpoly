d98d84423586b8e73a20b1a38f75fb379e9691b1
Pluto <Pluto@172.16.52.199>
2017-10-25 09:40:46 +0200

    Würfellogik implementiert

--- src/de/dhpoly/wuerfel/control/WuerfelImpl.java
+++ src/de/dhpoly/wuerfel/control/WuerfelImpl.java
@@ -1,5 +1,7 @@
 package de.dhpoly.wuerfel.control;
 
+import java.util.Random;
+
 import de.dhpoly.wuerfel.Wuerfel;
 
 public class WuerfelImpl implements Wuerfel
@@ -8,15 +10,20 @@ public class WuerfelImpl implements Wuerfel
 	@Override
 	public int wuerfeln()
 	{
-		// TODO Auto-generated method stub
-		return 0;
+		Random r = new Random();
+		int erg = 1 + r.nextInt(6);
+		return erg;
 	}
 
 	@Override
 	public boolean isPasch(int zahl1, int zahl2)
 	{
-		// TODO Auto-generated method stub
-		return false;
+		if(zahl1 == zahl2){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
 
 }
