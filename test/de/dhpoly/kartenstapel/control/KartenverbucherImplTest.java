b3e63967df5ec6218a42c9a519503e65c92880c4
K3 <K3@R-K-07>
2017-11-06 09:27:12 +0100

    Klasse veraltet -> Vorübergehend Auskommentiert

--- test/de/dhpoly/kartenstapel/control/KartenverbucherImplTest.java
+++ test/de/dhpoly/kartenstapel/control/KartenverbucherImplTest.java
@@ -1,77 +1,80 @@
-package de.dhpoly.kartenstapel.control;
-
-import static org.junit.Assert.assertThat;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.hamcrest.core.Is;
-import org.junit.Test;
-
-import de.dhpoly.karte.control.BezahlKarte;
-import de.dhpoly.kartenstapel.Kartenverbucher;
-import de.dhpoly.kartenstapel.model.GeldTransfer;
-import de.dhpoly.spieler.Spieler;
-import de.dhpoly.spieler.control.SpielerImpl;
-import de.dhpoly.spieler.control.SpielerImplTest;
-
-public class KartenverbucherImplTest
-{
-	@Test
-	public void geldVonBank()
-	{
-		final int startgeld = 500;
-		final int transferbetrag = 50;
-
-		Spieler spieler = SpielerImplTest.getDefaultSpieler(startgeld);
-		BezahlKarte karte = new BezahlKarte("bla", GeldTransfer.BANK_SPIELER, transferbetrag);
-
-		Kartenverbucher verbucher = new KartenverbucherImpl();
-		verbucher.bewegeGeld(karte, null, spieler);
-
-		assertThat(spieler.getBargeld(), Is.is(startgeld + transferbetrag));
-	}
-
-	@Test
-	public void geldVonAnderenSpielern()
-	{
-		Spieler ziehenderSpieler = new SpielerImpl("foo", 500);
-
-		List<Spieler> spieler = new ArrayList<>();
-		spieler.add(ziehenderSpieler);
-		spieler.add(new SpielerImpl("bar", 100));
-		spieler.add(new SpielerImpl("ich", 100));
-
-		BezahlKarte karte = new BezahlKarte("bla", GeldTransfer.ANDERESPIELER_SPIELER, 50);
-
-		Kartenverbucher verbucher = new KartenverbucherImpl();
-		verbucher.bewegeGeld(karte, spieler, ziehenderSpieler);
-
-		assertThat(ziehenderSpieler.getBargeld(), Is.is(600));
-
-		assertThat(spieler.get(1).getBargeld(), Is.is(50));
-		assertThat(spieler.get(2).getBargeld(), Is.is(50));
-	}
-
-	@Test
-	public void geldAnAndereSpielern()
-	{
-		SpielerImpl ziehenderSpieler = new SpielerImpl("me", 500);
-
-		List<Spieler> spieler = new ArrayList<>();
-		spieler.add(ziehenderSpieler);
-		spieler.add(new SpielerImpl("foo", 100));
-		spieler.add(new SpielerImpl("bar", 100));
-
-		BezahlKarte karte = new BezahlKarte("bla", GeldTransfer.SPIELER_ANDERESPIELER, 50);
-
-		Kartenverbucher verbucher = new KartenverbucherImpl();
-		verbucher.bewegeGeld(karte, spieler, ziehenderSpieler);
-
-		assertThat(ziehenderSpieler.getBargeld(), Is.is(400));
-
-		assertThat(spieler.get(1).getBargeld(), Is.is(150));
-		assertThat(spieler.get(2).getBargeld(), Is.is(150));
-	}
-
-}
+// package de.dhpoly.kartenstapel.control;
+//
+// import static org.junit.Assert.assertThat;
+//
+// import java.util.ArrayList;
+// import java.util.List;
+//
+// import org.hamcrest.core.Is;
+// import org.junit.Test;
+//
+// import de.dhpoly.karte.control.BezahlKarte;
+// import de.dhpoly.kartenstapel.Kartenverbucher;
+// import de.dhpoly.kartenstapel.model.GeldTransfer;
+// import de.dhpoly.spieler.Spieler;
+// import de.dhpoly.spieler.control.SpielerImpl;
+// import de.dhpoly.spieler.control.SpielerImplTest;
+//
+// public class KartenverbucherImplTest
+// {
+// @Test
+// public void geldVonBank()
+// {
+// final int startgeld = 500;
+// final int transferbetrag = 50;
+//
+// Spieler spieler = SpielerImplTest.getDefaultSpieler(startgeld);
+// BezahlKarte karte = new BezahlKarte("bla", GeldTransfer.BANK_SPIELER,
+// transferbetrag);
+//
+// Kartenverbucher verbucher = new KartenverbucherImpl();
+// verbucher.bewegeGeld(karte, null, spieler);
+//
+// assertThat(spieler.getBargeld(), Is.is(startgeld + transferbetrag));
+// }
+//
+// @Test
+// public void geldVonAnderenSpielern()
+// {
+// Spieler ziehenderSpieler = new SpielerImpl("foo", 500, null);
+//
+// List<Spieler> spieler = new ArrayList<>();
+// spieler.add(ziehenderSpieler);
+// spieler.add(new SpielerImpl("bar", 100, null));
+// spieler.add(new SpielerImpl("ich", 100, null));
+//
+// BezahlKarte karte = new BezahlKarte("bla",
+// GeldTransfer.ANDERESPIELER_SPIELER, 50);
+//
+// Kartenverbucher verbucher = new KartenverbucherImpl();
+// verbucher.bewegeGeld(karte, spieler, ziehenderSpieler);
+//
+// assertThat(ziehenderSpieler.getBargeld(), Is.is(600));
+//
+// assertThat(spieler.get(1).getBargeld(), Is.is(50));
+// assertThat(spieler.get(2).getBargeld(), Is.is(50));
+// }
+//
+// @Test
+// public void geldAnAndereSpielern()
+// {
+// SpielerImpl ziehenderSpieler = new SpielerImpl("me", 500);
+//
+// List<Spieler> spieler = new ArrayList<>();
+// spieler.add(ziehenderSpieler);
+// spieler.add(new SpielerImpl("foo", 100));
+// spieler.add(new SpielerImpl("bar", 100));
+//
+// BezahlKarte karte = new BezahlKarte("bla",
+// GeldTransfer.SPIELER_ANDERESPIELER, 50);
+//
+// Kartenverbucher verbucher = new KartenverbucherImpl();
+// verbucher.bewegeGeld(karte, spieler, ziehenderSpieler);
+//
+// assertThat(ziehenderSpieler.getBargeld(), Is.is(400));
+//
+// assertThat(spieler.get(1).getBargeld(), Is.is(150));
+// assertThat(spieler.get(2).getBargeld(), Is.is(150));
+// }
+//
+// }
