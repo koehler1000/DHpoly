45d582d6413d0896c22ee6af8b2f417fe2e7f27f
K3 <K3@R-K-07>
2017-11-06 09:46:34 +0100

    Rueckenkarte implementiert

--- src/de/dhpoly/karte/control/RueckenKarte.java
+++ src/de/dhpoly/karte/control/RueckenKarte.java
@@ -1,22 +1,27 @@
 package de.dhpoly.karte.control;
 
+import de.dhpoly.feld.Feld;
 import de.dhpoly.karte.Karte;
 
 public class RueckenKarte implements Karte
 {
+	private Feld zielFeld;
+
+	public RueckenKarte(Feld zielFeld)
+	{
+		this.zielFeld = zielFeld;
+	}
 
 	@Override
 	public String getBeschreibung()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return "Rücken Sie auf das Feld: " + System.lineSeparator() + zielFeld.getBeschriftung();
 	}
 
 	@Override
 	public String getTitel()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return "Sie werden verschoben...";
 	}
 
 }
