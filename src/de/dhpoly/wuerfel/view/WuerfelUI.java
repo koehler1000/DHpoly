d9931be5dbf5bd6daa9fb7c090ebaaf23873157f
K3 <K3@R-K-07>
2017-11-06 11:24:10 +0100

    Wuerfel modifiziert

--- src/de/dhpoly/wuerfel/view/WuerfelUI.java
+++ src/de/dhpoly/wuerfel/view/WuerfelUI.java
@@ -3,9 +3,13 @@ package de.dhpoly.wuerfel.view;
 import java.awt.Component;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.io.IOException;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+
+import de.dhpoly.fehler.control.FehlerImpl;
 
 public class WuerfelUI extends JPanel
 {
@@ -15,16 +19,31 @@ public class WuerfelUI extends JPanel
 	{
 		this.setLayout(new GridLayout(1, 2, 10, 10));
 
-		this.add(getWuerfel(ergebnis1));
-		this.add(getWuerfel(ergebnis2));
+		try
+		{
+			this.add(getWuerfel(ergebnis1));
+			this.add(getWuerfel(ergebnis2));
+		}
+		catch (IOException ex)
+		{
+			FehlerImpl.fehlerAufgetreten(ex);
+		}
 	}
 
-	private Component getWuerfel(int nummer)
+	private Component getWuerfel(int nummer) throws IOException
 	{
-		JTextArea wuerfel = new JTextArea();
-		wuerfel.setFont(new Font("arial", Font.BOLD, 30));
-		wuerfel.setEditable(false);
-		wuerfel.setText("" + nummer);
-		return wuerfel;
+		switch (nummer)
+		{
+			case 1:
+				JLabel lblBild = new JLabel();
+				return lblBild;
+			default:
+				JTextArea wuerfel = new JTextArea();
+				wuerfel.setFont(new Font("arial", Font.BOLD, 30));
+				wuerfel.setEditable(false);
+				wuerfel.setText("" + nummer);
+				return wuerfel;
+		}
+
 	}
 }
