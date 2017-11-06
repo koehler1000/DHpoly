c020d60cd8b5bb78467c7ce9d10b84d6df08ee73
K3 <K3@R-K-07>
2017-11-06 10:25:31 +0100

    Balancing-Variablen in Einstellungen angelegt

--- src/de/dhpoly/einstellungen/Einstellungen.java
+++ src/de/dhpoly/einstellungen/Einstellungen.java
@@ -2,6 +2,30 @@ package de.dhpoly.einstellungen;
 
 public interface Einstellungen
 {
+	void setKostenHausHolz(int kostenHausHolz);
+
+	int getKostenHausHolz();
+
+	void setKostenHausGeld(int kostenHausGeld);
+
+	int getKostenHausGeld();
+
+	void setRessourcenErtrag(int ressourcenErtrag);
+
+	int getRessourcenErtrag();
+
+	void setStartguthaben(int startguthaben);
+
+	int getStartguthaben();
+
+	void setBetragPassierenLos(int betragPassierenLos);
+
+	int getBetragPassierenLos();
+
+	void setKostenHausStein(int kostenHausStein);
+
+	int getKostenHausStein();
+
 	int getBetragBetretenLos();
 
 	void setBetragBetretenLos(int betragBetretenLos);
--- src/de/dhpoly/einstellungen/control/EinstellungenImpl.java
+++ src/de/dhpoly/einstellungen/control/EinstellungenImpl.java
@@ -4,7 +4,17 @@ import de.dhpoly.einstellungen.Einstellungen;
 
 public class EinstellungenImpl implements Einstellungen
 {
-	int betragBetretenLos = 2000;
+	private int startguthaben = 1000;
+
+	private int betragBetretenLos = 100;
+	private int betragPassierenLos = 100;
+
+	// Kosten je auf Seite 1 -> Seite 2 = Kosten von Seite 1 * 2
+	private int kostenHausStein = 10;
+	private int kostenHausGeld = 100;
+	private int kostenHausHolz = 10;
+
+	private int ressourcenErtrag;
 
 	@Override
 	public int getBetragBetretenLos()
@@ -16,5 +26,77 @@ public class EinstellungenImpl implements Einstellungen
 	public void setBetragBetretenLos(int betragBetretenLos)
 	{
 		this.betragBetretenLos = betragBetretenLos;
+	}
+
+	@Override
+	public int getKostenHausStein()
+	{
+		return kostenHausStein;
+	}
+
+	@Override
+	public void setKostenHausStein(int kostenHausStein)
+	{
+		this.kostenHausStein = kostenHausStein;
+	}
+
+	@Override
+	public int getBetragPassierenLos()
+	{
+		return betragPassierenLos;
+	}
+
+	@Override
+	public void setBetragPassierenLos(int betragPassierenLos)
+	{
+		this.betragPassierenLos = betragPassierenLos;
+	}
+
+	@Override
+	public int getStartguthaben()
+	{
+		return startguthaben;
+	}
+
+	@Override
+	public void setStartguthaben(int startguthaben)
+	{
+		this.startguthaben = startguthaben;
+	}
+
+	@Override
+	public int getRessourcenErtrag()
+	{
+		return ressourcenErtrag;
+	}
+
+	@Override
+	public void setRessourcenErtrag(int ressourcenErtrag)
+	{
+		this.ressourcenErtrag = ressourcenErtrag;
+	}
+
+	@Override
+	public int getKostenHausGeld()
+	{
+		return kostenHausGeld;
+	}
+
+	@Override
+	public void setKostenHausGeld(int kostenHausGeld)
+	{
+		this.kostenHausGeld = kostenHausGeld;
+	}
+
+	@Override
+	public int getKostenHausHolz()
+	{
+		return kostenHausHolz;
+	}
+
+	@Override
+	public void setKostenHausHolz(int kostenHausHolz)
+	{
+		this.kostenHausHolz = kostenHausHolz;
 	}
 }
