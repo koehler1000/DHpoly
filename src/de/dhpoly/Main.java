f1fa4b6a590eb8172a3f00473952a263aeccd6c5
K3 <K3@R-K-07>
2017-11-06 09:28:09 +0100

    Fehler behoben

--- src/de/dhpoly/Main.java
+++ src/de/dhpoly/Main.java
@@ -1,14 +1,11 @@
 package de.dhpoly;
 
-import de.dhpoly.spieler.Spieler;
-import de.dhpoly.spieler.control.SpielerImpl;
-
 public class Main
 {
 	public static void main(String[] args)
 	{
-		Spieler s1 = new SpielerImpl("Tester1", 1000);
-		Spieler s2 = new SpielerImpl("Tester2", 1000);
+		// Spieler s1 = new SpielerImpl("Tester1", 1000);
+		// Spieler s2 = new SpielerImpl("Tester2", 1000);
 		// TODO ergänzen
 	}
 }
