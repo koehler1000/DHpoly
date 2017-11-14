2944c821a30f46fd09f0ca9b5749f5ee97d7924c
Sven <Sven@DESKTOP-4GUCGUP>
2017-11-14 22:39:33 +0100

    grafische Darstellung der Ressourcen Holz und Stein eingefügt
    #DHP-7

--- src/de/dhpoly/feld/view/RessourcenfeldUI.java
+++ src/de/dhpoly/feld/view/RessourcenfeldUI.java
@@ -3,14 +3,21 @@ package de.dhpoly.feld.view;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
 
+import de.dhpoly.fehler.control.FehlerImpl;
 import de.dhpoly.feld.control.Ressourcenfeld;
+import de.dhpoly.feld.model.Ressource;
 
 public class RessourcenfeldUI extends JPanel implements Observer
 {
@@ -19,6 +26,8 @@ public class RessourcenfeldUI extends JPanel implements Observer
 	private Component pnlSpieler = new JPanel();
 
 	private Ressourcenfeld feld;
+	
+	private JLabel ressourcenbild = new JLabel();
 
 	public RessourcenfeldUI(Ressourcenfeld feld)
 	{
@@ -28,7 +37,13 @@ public class RessourcenfeldUI extends JPanel implements Observer
 		this.setBorder(new LineBorder(Color.BLACK));
 
 		this.add(new JLabel(feld.getBeschriftung()), BorderLayout.NORTH);
-
+		
+		this.add(ressourcenbild);
+		ressourcenbild.setIcon(new ImageIcon(getRessourcenBild(feld.getRessource()).getImage().getScaledInstance(60, 60, Image.SCALE_DEFAULT)));
+		//aktuell werden die pngs immer auf 60*60 gesized
+		
+		hintergrundfarbeSetzen(this);
+		
 		feld.addObserver(this);
 	}
 
@@ -43,6 +58,35 @@ public class RessourcenfeldUI extends JPanel implements Observer
 		this.remove(pnlSpieler);
 		pnlSpieler = new FeldUI().getSpieler(feld.getSpielerAufFeld());
 		this.add(pnlSpieler);
+		hintergrundfarbeSetzen(pnlSpieler);
+	}
+	
+	
+	private void hintergrundfarbeSetzen(Component pnlSpieler2) {
+		switch (feld.getRessource()) {
+		case HOLZ:
+			pnlSpieler2.setBackground(Color.getHSBColor(33, 94, 78));
+			break;
+		case STEIN:
+			pnlSpieler2.setBackground(Color.GRAY);
+			
+		default:
+			break;
+		}
+	}
+	
+	private ImageIcon getRessourcenBild(Ressource res)
+	{
+		
+		try
+		{
+			return new ImageIcon(ImageIO.read(new File(res.toString() + ".png")));
+		}
+		catch (IOException ex)
+		{
+			FehlerImpl.fehlerAufgetreten(ex);
+			return new ImageIcon();
+		}
 	}
 
 }
