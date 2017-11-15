9487b54ab452f4efbcc9478c38fa6101f5703927
K3 <K3@R-K-07>
2017-11-15 09:53:15 +0100

    Grundinformationen (Prototyp)
    #DHP-17

---
+++ viewtest/de/dhpoly/feld/view/StrasseInfoUITest.java
@@ -0,0 +1,19 @@
+package de.dhpoly.feld.view;
+
+import javax.swing.JFrame;
+
+import de.dhpoly.feld.control.Strasse;
+
+public class StrasseInfoUITest
+{
+	public static void main(String[] args)
+	{
+		JFrame frame = new JFrame("StrasseInfoUITest");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(100, 200);
+
+		frame.add(new StrasseInfoUI(new Strasse(null, 1000, new int[] { 10, 30, 40, 80, 100 }, 1, 1, "Teststrasse")));
+
+		frame.setVisible(true);
+	}
+}
