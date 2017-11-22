069735e03b2db6bac203be777a0a9371cc974bba
K3 <K3@R-K-07>
2017-11-22 11:28:02 +0100

    Refactoring -> RessourcenDatensatz verwendet

--- viewtest/de/dhpoly/kartenstapel/view/KartenOberflaecheVorschau.java
+++ viewtest/de/dhpoly/kartenstapel/view/KartenOberflaecheVorschau.java
@@ -4,14 +4,15 @@ import org.junit.Test;
 
 import de.dhpoly.karte.control.BezahlKarte;
 import de.dhpoly.kartenstapel.model.GeldTransfer;
+import de.dhpoly.ressource.control.RessourcenDatensatzImpl;
+import de.dhpoly.ressource.model.Ressource;
 
 public class KartenOberflaecheVorschau
 {
-
 	@Test
 	public void vorschau()
 	{
-		new KartenOberflaeche(new BezahlKarte("Die Steuer wird fällig, zahle 1000€", GeldTransfer.BANK_SPIELER, 1000));
+		new KartenOberflaeche(new BezahlKarte("Die Steuer wird fällig, zahle 1000€", GeldTransfer.BANK_SPIELER,
+				new RessourcenDatensatzImpl(Ressource.GELD, 1000)));
 	}
-
 }
