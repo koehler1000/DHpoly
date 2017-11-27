2c68be310ffde3894e54baf1091270d349102241
K3 <K3@R-K-07>
2017-11-27 11:06:51 +0100

    Anzeige für mehrere Häuser
    #DHP-88

---
+++ src/de/dhpoly/feld/view/HaeuserUI.java
@@ -0,0 +1,20 @@
+package de.dhpoly.feld.view;
+
+import java.awt.GridLayout;
+import java.util.List;
+
+import javax.swing.JPanel;
+
+public class HaeuserUI extends JPanel
+{
+	private static final long serialVersionUID = 1L;
+
+	public HaeuserUI(List<HausUI> UIs)
+	{
+		this.setLayout(new GridLayout(0, 0, 10, 10));
+		for (HausUI hausUI : UIs)
+		{
+			this.add(hausUI);
+		}
+	}
+}
