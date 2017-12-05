14b8f83ef2bb3ff5339676b3ec5d25c4758c1963
K3 <K3@R-K-07>
2017-12-05 12:04:00 +0100

    JUnit-Test eingefügt

--- cucumber-test/Stepdefs.java
+++ cucumber-test/Stepdefs.java
@@ -3,121 +3,144 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import de.dhpoly.spieler.control.SpielerImplTest;
 
-public class Stepdefs { 
-    
-    @Given("^It is my turn$")
-    public void it_is_my_turn() throws Throwable {
-        throw new PendingException();
-    }
+public class Stepdefs
+{
 
-    @When("^I choose a player$")
-    public void i_choose_a_player() throws Throwable {
-        throw new PendingException();
-    }
+	@Given("^It is my turn$")
+	public void it_is_my_turn() throws Throwable
+	{
+		new SpielerImplTest().testaktuellerSpieler();
+	}
 
-    @Then("^A trading offer is sent to the player I selected$")
-    public void a_trading_offer_is_sent_to_the_player_i_selected() throws Throwable {
-        throw new PendingException();
-    }
+	@When("^I choose a player$")
+	public void i_choose_a_player() throws Throwable
+	{
+		throw new PendingException();
+	}
 
-    @Then("^The other player accepts the trading offer$")
-    public void the_other_player_accepts_the_trading_offer() throws Throwable {
-        throw new PendingException();
-    }
+	@Then("^A trading offer is sent to the player I selected$")
+	public void a_trading_offer_is_sent_to_the_player_i_selected() throws Throwable
+	{
+		throw new PendingException();
+	}
 
-    @Then("^The ownerships should have transfered$")
-    public void the_ownerships_should_have_transfered() throws Throwable {
-        throw new PendingException();
-    }
+	@Then("^The other player accepts the trading offer$")
+	public void the_other_player_accepts_the_trading_offer() throws Throwable
+	{
+		throw new PendingException();
+	}
 
-    @Then("^The other player denies the trading offer$")
-    public void the_other_player_denies_the_trading_offer() throws Throwable {
-        throw new PendingException();
-    }
+	@Then("^The ownerships should have transfered$")
+	public void the_ownerships_should_have_transfered() throws Throwable
+	{
+		throw new PendingException();
+	}
 
-    @Then("^There should be a windows displaying \"([^\"]*)\"$")
-    public void there_should_be_a_windows_displaying_something(String strArg1) throws Throwable {
-        throw new PendingException();
-    }
+	@Then("^The other player denies the trading offer$")
+	public void the_other_player_denies_the_trading_offer() throws Throwable
+	{
+		throw new PendingException();
+	}
 
-    @And("^I choose a street$")
-    public void i_choose_a_street() throws Throwable {
-        throw new PendingException();
-    }
-    
-    @And("^I choose the ressources$")
-    public void i_choose_the_ressources() throws Throwable {
-        throw new PendingException();
-    }
+	@Then("^There should be a windows displaying \"([^\"]*)\"$")
+	public void there_should_be_a_windows_displaying_something(String strArg1) throws Throwable
+	{
+		throw new PendingException();
+	}
 
-    @And("^I choose the currency$")
-    public void i_choose_the_currency() throws Throwable {
-        throw new PendingException();
-    }
-    
-    @Given("^I have to pay my debts$")
-    public void i_have_to_pay_my_debts() throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
-    }
+	@And("^I choose a street$")
+	public void i_choose_a_street() throws Throwable
+	{
+		throw new PendingException();
+	}
 
-    @Given("^I have enough money$")
-    public void i_have_enough_money() throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
-    }
+	@And("^I choose the ressources$")
+	public void i_choose_the_ressources() throws Throwable
+	{
+		throw new PendingException();
+	}
 
-    @Then("^I withdraw money$")
-    public void i_withdraw_money() throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
-    }
+	@And("^I choose the currency$")
+	public void i_choose_the_currency() throws Throwable
+	{
+		throw new PendingException();
+	}
 
-    @Then("^I pay my debts$")
-    public void i_pay_my_debts() throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
-    }
+	@Given("^I have to pay my debts$")
+	public void i_have_to_pay_my_debts() throws Exception
+	{
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
 
-    @Given("^I do not have enough money$")
-    public void i_do_not_have_enough_money() throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
-    }
+	@Given("^I have enough money$")
+	public void i_have_enough_money() throws Exception
+	{
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
 
-    @Given("^I have enough property$")
-    public void i_have_enough_property() throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
-    }
+	@Then("^I withdraw money$")
+	public void i_withdraw_money() throws Exception
+	{
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
 
-    @Then("^I have to sell property$")
-    public void i_have_to_sell_property() throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
-    }
+	@Then("^I pay my debts$")
+	public void i_pay_my_debts() throws Exception
+	{
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
 
-    @Given("^I do not have enough property$")
-    public void i_do_not_have_enough_property() throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
-    }
+	@Given("^I do not have enough money$")
+	public void i_do_not_have_enough_money() throws Exception
+	{
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
 
-    @Then("^I see the message \"([^\"]*)\"$")
-    public void i_see_the_message(String arg1) throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
-    }
-    
-    @And("^The other player will deny my trading offer$")
-    public void the_other_player_will_deny_my_trading_offer() throws Throwable {
-        throw new PendingException();
-    }
+	@Given("^I have enough property$")
+	public void i_have_enough_property() throws Exception
+	{
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
 
-    @And("^The other player will accept my trading offer$")
-    public void the_other_player_will_accept_my_trading_offer() throws Throwable {
-        throw new PendingException();
-    }
+	@Then("^I have to sell property$")
+	public void i_have_to_sell_property() throws Exception
+	{
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Given("^I do not have enough property$")
+	public void i_do_not_have_enough_property() throws Exception
+	{
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^I see the message \"([^\"]*)\"$")
+	public void i_see_the_message(String arg1) throws Exception
+	{
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@And("^The other player will deny my trading offer$")
+	public void the_other_player_will_deny_my_trading_offer() throws Throwable
+	{
+		throw new PendingException();
+	}
+
+	@And("^The other player will accept my trading offer$")
+	public void the_other_player_will_accept_my_trading_offer() throws Throwable
+	{
+		throw new PendingException();
+	}
 
 }
