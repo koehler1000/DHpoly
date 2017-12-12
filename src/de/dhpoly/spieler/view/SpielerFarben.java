f9f4d89f69e2ae29dc087ff0a28d92f81cfa4259
K3 <K3@R-K-07>
2017-12-12 11:33:30 +0100

    Farben für Präsentation angepasst
    #DHP-125

--- src/de/dhpoly/spieler/view/SpielerFarben.java
+++ src/de/dhpoly/spieler/view/SpielerFarben.java
@@ -4,7 +4,8 @@ import java.awt.Color;
 
 public class SpielerFarben
 {
-	private static Color[] farben = new Color[] { Color.RED, Color.BLUE, Color.GREEN };
+	private static Color[] farben = new Color[] { new Color (255,155,0), Color.YELLOW, Color.GREEN, new Color(255, 50, 50),
+			new Color(50, 50, 255), new Color(50, 255, 50) };
 
 	public static Color getSpielerfarbe(int spielerID)
 	{
