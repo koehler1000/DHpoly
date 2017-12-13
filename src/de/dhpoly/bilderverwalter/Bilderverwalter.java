78bab80d4a927fd41c876326c838a67fd20e1e06
K3 <K3@R-K-07>
2017-12-13 13:39:54 +0100

    Deutlich weniger Fehler + Massive Performanceverbesserung

--- src/de/dhpoly/bilderverwalter/Bilderverwalter.java
+++ src/de/dhpoly/bilderverwalter/Bilderverwalter.java
@@ -2,6 +2,8 @@ package de.dhpoly.bilderverwalter;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
@@ -18,6 +20,8 @@ public class Bilderverwalter
 	public static final String RESSOURCE_GELD = "ressourcen\\geld.png";
 
 	private static final String WUERFEL = "wuerfel//wuerfel_%.png"; // % wird zu Nummer des Würfels
+
+	private static Map<String, ImageIcon> bilder = new HashMap<>();
 
 	public static ImageIcon getBild(Ressource ressource)
 	{
@@ -42,14 +46,23 @@ public class Bilderverwalter
 
 	public static ImageIcon getBild(String pfad)
 	{
+		if (bilder.containsKey(pfad))
+		{
+			return bilder.get(pfad);
+		}
+
 		try
 		{
-			return new ImageIcon(ImageIO.read(new File(".\\pics\\default\\" + pfad)));
+			ImageIcon icon = new ImageIcon(ImageIO.read(new File(".\\pics\\default\\" + pfad)));
+			bilder.put(pfad, icon);
+			return icon;
 		}
 		catch (IOException ex)
 		{
 			FehlerImpl.stillerFehler(ex + " (" + pfad + ")");
-			return new ImageIcon();
+			ImageIcon icon = new ImageIcon();
+			bilder.put(pfad, icon);
+			return icon;
 		}
 	}
 }
