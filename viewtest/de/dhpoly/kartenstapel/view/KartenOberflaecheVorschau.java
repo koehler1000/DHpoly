99e7b310daa73956bab6a72378c463353ab74419
K3 <K3@R-K-07>
2018-04-09 10:06:32 +0200

    KartenOberflaeche refactored
    #DHP-110

--- viewtest/de/dhpoly/kartenstapel/view/KartenOberflaecheVorschau.java
+++ viewtest/de/dhpoly/kartenstapel/view/KartenOberflaecheVorschau.java
@@ -2,8 +2,11 @@ package de.dhpoly.kartenstapel.view;
 
 import org.junit.Test;
 
+import de.dhpoly.bilderverwalter.Bilderverwalter;
 import de.dhpoly.karte.control.BezahlKarte;
+import de.dhpoly.karte.view.KarteUI;
 import de.dhpoly.kartenstapel.model.GeldTransfer;
+import de.dhpoly.oberflaeche.view.Fenster;
 import de.dhpoly.ressource.control.RessourcenDatensatzImpl;
 import de.dhpoly.ressource.model.Ressource;
 
@@ -12,7 +15,8 @@ public class KartenOberflaecheVorschau
 	@Test
 	public void vorschau()
 	{
-		new KartenOberflaeche(new BezahlKarte("Die Steuer wird fällig, zahle 1000€", GeldTransfer.BANK_SPIELER,
-				new RessourcenDatensatzImpl(Ressource.GELD, 1000)));
+		new Fenster(new Bilderverwalter())
+				.zeigeComponente(new KarteUI(new BezahlKarte("Die Steuer wird fällig, zahle 1000€",
+						GeldTransfer.BANK_SPIELER, new RessourcenDatensatzImpl(Ressource.GELD, 1000))));
 	}
 }
