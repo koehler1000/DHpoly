48de657fffee444db976b955e2725a9257362a02
K3 <K3@R-K-07>
2018-04-11 11:42:09 +0200

    Nachricht erzeugt
    #DHP-110

---
+++ src/de/dhpoly/nachricht/model/Nachricht.java
@@ -0,0 +1,20 @@
+package de.dhpoly.nachricht.model;
+
+import de.dhpoly.datenobjekt.Datenobjekt;
+
+public class Nachricht extends Datenobjekt
+{
+	private static final long serialVersionUID = 1L;
+
+	private String nachrichtentext = "Keine Nachricht angegeben";
+
+	public Nachricht(String nachrichtentext)
+	{
+		this.nachrichtentext = nachrichtentext;
+	}
+
+	public String getNachricht()
+	{
+		return nachrichtentext;
+	}
+}
