250790e809aa1787961cc56cb33cb0642aaac706
K3 <K3@R-K-07>
2018-04-12 10:01:18 +0200

    Refactoring (mit Spielansicht)
    #DHP-110

--- viewtest/de/dhpoly/karte/view/KarteUITest.java
+++ viewtest/de/dhpoly/karte/view/KarteUITest.java
@@ -1,7 +1,5 @@
 package de.dhpoly.karte.view;
 
-import org.junit.Test;
-
 import de.dhpoly.karte.control.BezahlKarte;
 import de.dhpoly.kartenstapel.model.GeldTransfer;
 import de.dhpoly.ressource.control.RessourcenDatensatzImpl;
@@ -10,8 +8,7 @@ import de.dhpoly.utils.Spielansicht;
 
 public class KarteUITest // NOSONAR
 {
-	@Test
-	public void vorschau()
+	public static void main(String[] args)
 	{
 		Spielansicht.zeige(new KarteUI(new BezahlKarte("Die Steuer wird fällig, zahle 1000€", GeldTransfer.BANK_SPIELER,
 				new RessourcenDatensatzImpl(Ressource.GELD, 1000)), Spielansicht.getSpielfeldAnsicht()));
