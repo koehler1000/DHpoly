35a6788b03f5383de682af09eda734c19e9c2ad6
K3 <K3@R-K-07>
2018-04-12 09:55:43 +0200

    Refactoring (mit Spielansicht)
    #DHP-110

--- viewtest/de/dhpoly/feld/view/StrasseKaufenUITest.java
+++ viewtest/de/dhpoly/feld/view/StrasseKaufenUITest.java
@@ -2,11 +2,13 @@ package de.dhpoly.feld.view;
 
 import de.dhpoly.feld.control.StrasseTest;
 import de.dhpoly.spieler.control.SpielerImplTest;
+import de.dhpoly.utils.Spielansicht;
 
 public class StrasseKaufenUITest // NOSONAR
 {
 	public static void main(String[] args)
 	{
-		new StrasseKaufenUI(StrasseTest.getDefaultStrasse(), SpielerImplTest.getDefaultSpieler(), null);
+		Spielansicht.zeige(new StrasseKaufenUI(StrasseTest.getDefaultStrasse(), SpielerImplTest.getDefaultSpieler(),
+				Spielansicht.getSpielfeldAnsicht()));
 	}
 }
