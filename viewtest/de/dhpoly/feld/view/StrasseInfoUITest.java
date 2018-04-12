444084acf6bb9949e3e1ebde932366a54085807e
K3 <K3@R-K-07>
2018-04-12 09:54:30 +0200

    Refactoring (mit Spielansicht)
    #DHP-110

--- viewtest/de/dhpoly/feld/view/StrasseInfoUITest.java
+++ viewtest/de/dhpoly/feld/view/StrasseInfoUITest.java
@@ -1,15 +1,14 @@
 package de.dhpoly.feld.view;
 
-import de.dhpoly.bilderverwalter.Bilderverwalter;
 import de.dhpoly.einstellungen.model.EinstellungenImpl;
 import de.dhpoly.feld.control.Strasse;
-import de.dhpoly.oberflaeche.view.Fenster;
+import de.dhpoly.utils.Spielansicht;
 
 public class StrasseInfoUITest // NOSONAR
 {
 	public static void main(String[] args)
 	{
-		new Fenster(new Bilderverwalter()).zeigeComponente(new StrasseInfoUI(new Strasse(null, 1000,
-				new int[] { 10, 30, 40, 80, 100 }, new EinstellungenImpl().getHauskosten(1), 1, "Teststrasse"), null));
+		Spielansicht.zeige(new StrasseInfoUI(new Strasse(null, 1000, new int[] { 10, 30, 40, 80, 100 },
+				new EinstellungenImpl().getHauskosten(1), 1, "Teststrasse"), Spielansicht.getSpielfeldAnsicht()));
 	}
 }
