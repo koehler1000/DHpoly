47b39427f26e84307cc0160082af3c1839ed668f
K3 <K3@R-K-07>
2018-04-13 10:32:46 +0200

    Deprecated gesetzt
    #DHP-110

--- src/de/dhpoly/fehler/FehlerVerarbeiter.java
+++ src/de/dhpoly/fehler/FehlerVerarbeiter.java
@@ -5,6 +5,7 @@ import de.dhpoly.oberflaeche.view.SpielfeldAnsicht;
 
 public interface FehlerVerarbeiter
 {
+	@Deprecated
 	static void fehlerAufgetreten(de.dhpoly.fehler.model.Fehler fehler, SpielfeldAnsicht ansicht)
 	{
 		new FehlerVerarbeiterImpl(ansicht).fehlerAufgetreten(fehler);
