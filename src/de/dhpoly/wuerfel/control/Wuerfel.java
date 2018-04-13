1af2dfd4fd7579416e0668aaf869ec27c07a8206
K3 <K3@R-K-07>
2018-04-13 11:27:51 +0200

    Logik aus Datenobjekt entfernt
    #DHP-110

--- src/de/dhpoly/wuerfel/control/Wuerfel.java
+++ src/de/dhpoly/wuerfel/control/Wuerfel.java
@@ -1,7 +1,5 @@
 package de.dhpoly.wuerfel.control;
 
-import java.util.Random;
-
 import observerpattern.Beobachtbarer;
 
 public class Wuerfel extends Beobachtbarer
@@ -11,13 +9,6 @@ public class Wuerfel extends Beobachtbarer
 	public Wuerfel(int zahl)
 	{
 		this.zahl = zahl;
-	}
-
-	@Deprecated
-	public void wuerfeln()
-	{
-		Random r = new Random();
-		setZahl(1 + r.nextInt(6));
 	}
 
 	public void setZahl(int zahl)
