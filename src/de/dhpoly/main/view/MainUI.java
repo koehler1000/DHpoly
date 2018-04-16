dd597a05597a0657908b7f70bebf6e232cdf0da4
K3 <K3@R-K-07>
2018-04-16 08:54:59 +0200

    Spieler koennen jetzt auch ohne Angabe von Namen hinzugefuegt werden.
    #DHP-76

--- src/de/dhpoly/main/view/MainUI.java
+++ src/de/dhpoly/main/view/MainUI.java
@@ -1,5 +1,6 @@
 package de.dhpoly.main.view;
 
+import java.awt.Component;
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
@@ -11,37 +12,67 @@ import de.dhpoly.oberflaeche.view.Fenster;
 import de.dhpoly.spiel.Spiel;
 import de.dhpoly.spiel.control.SpielImpl;
 import de.dhpoly.spielfeld.model.Standardspielfeld;
+import observerpattern.Beobachter;
 
-public class MainUI extends JPanel
+public class MainUI extends JPanel implements Beobachter
 {
 	private static final long serialVersionUID = 1L;
+
+	private JTextArea txtName;
+	private transient Spiel spiel;
 
 	public MainUI(Fenster fenster)
 	{
 		this.setLayout(new GridLayout(2, 3));
 
-		Spiel spiel = new SpielImpl(fenster);
+		spiel = new SpielImpl(fenster);
 		spiel.setFelder(new Standardspielfeld().getStandardSpielfeld());
+		spiel.addBeobachter(this);
 
-		this.add(ElementFactory.getTextFeldUeberschrift("Spieler"));
+		this.add(getSpielerVerwalterPanel(spiel));
+		this.add(getSpielVerwalterPanel(fenster, spiel));
 
-		JTextArea txtName = ElementFactory.getTextFeld("Name", true);
-		this.add(txtName);
+		fenster.zeigeComponente(this, "Spielersteller");
+	}
 
-		JButton butHinzu = ElementFactory.getButtonUeberschrift("+");
-		butHinzu.addActionListener(e -> spiel.fuegeSpielerHinzu(txtName.getText()));
-		this.add(butHinzu);
+	private Component getSpielVerwalterPanel(Fenster fenster, Spiel spiel)
+	{
+		JPanel pnl = ElementFactory.erzeugePanel();
+		pnl.setLayout(new GridLayout());
 
-		this.add(ElementFactory.getTextFeldUeberschrift("Spiel verwalten"));
-		this.add(ElementFactory.getTextFeldUeberschrift(""));
+		pnl.add(ElementFactory.getTextFeldUeberschrift("Spiel verwalten"));
+		pnl.add(ElementFactory.getTextFeldUeberschrift(""));
 
 		JButton butStart = ElementFactory.getButtonUeberschrift("Spiel starten");
 		butStart.addActionListener(e -> {
 			spiel.starteSpiel();
 			fenster.loescheKomponente(this);
 		});
-		this.add(butStart);
+		pnl.add(butStart);
 
-		fenster.zeigeComponente(this, "Spielersteller");
+		return pnl;
+	}
+
+	private Component getSpielerVerwalterPanel(Spiel spiel)
+	{
+		JPanel pnl = ElementFactory.erzeugePanel();
+		pnl.setLayout(new GridLayout());
+
+		pnl.add(ElementFactory.getTextFeldUeberschrift("Lokale Spieler"));
+
+		txtName = ElementFactory.getTextFeld("Spieler " + (spiel.getSpieler().size() + 1), true);
+		pnl.add(txtName);
+
+		JButton butHinzu = ElementFactory.getButtonUeberschrift("+");
+		butHinzu.addActionListener(e -> spiel.fuegeSpielerHinzu(txtName.getText()));
+		pnl.add(butHinzu);
+
+		return pnl;
+	}
+
+	@Override
+	public void update()
+	{
+		txtName.setText("Spieler " + (spiel.getSpieler().size() + 1));
 	}
 }
