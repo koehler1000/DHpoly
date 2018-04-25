a3f8cf3c3f80e8ef9a88a2aeb3c25bd0929c29be
K3 <K3@R-K-07>
2018-04-25 11:48:25 +0200

    Interface angepasst
    #DHP-166

--- src_client/de/dhpoly/netzwerk/NetzwerkClient.java
+++ src_client/de/dhpoly/netzwerk/NetzwerkClient.java
@@ -1,6 +1,13 @@
 package de.dhpoly.netzwerk;
 
+import de.dhpoly.datenobjekt.Datenobjekt;
+import de.dhpoly.oberflaeche.view.SpielfeldAnsicht;
+
 public interface NetzwerkClient
 {
+	void sende(String text);
 
+	void sende(Datenobjekt objekt);
+
+	void addAnsicht(SpielfeldAnsicht ansicht);
 }
