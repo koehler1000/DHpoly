0124e4884caaebf95c7b8a7a9d198e65e2abb8c2
K3 <K3@R-K-07>
2018-04-25 11:36:44 +0200

    Interface bearbeitet
    #DHP-166

--- src_server/de/dhpoly/netzwerk/NetzwerkServer.java
+++ src_server/de/dhpoly/netzwerk/NetzwerkServer.java
@@ -1,6 +1,13 @@
 package de.dhpoly.netzwerk;
 
+import de.dhpoly.datenobjekt.Datenobjekt;
+import de.dhpoly.spiel.Spiel;
+
 public interface NetzwerkServer
 {
+	void sende(Datenobjekt obj);
 
+	void sende(String string);
+
+	void addInteressent(Spiel spiel);
 }
