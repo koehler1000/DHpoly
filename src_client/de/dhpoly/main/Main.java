abcecdeb0e05440040e03644451f9243ba54081a
K3 <K3@R-K-07>
2018-05-01 11:59:09 +0200

    Prototyp

--- src_client/de/dhpoly/main/Main.java
+++ src_client/de/dhpoly/main/Main.java
@@ -1,8 +1,15 @@
 package de.dhpoly.main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import de.dhpoly.bilderverwalter.Bilderverwalter;
+import de.dhpoly.einstellungen.model.Einstellungen;
 import de.dhpoly.main.view.MainUI;
 import de.dhpoly.oberflaeche.view.Fenster;
+import de.dhpoly.spiel.model.SpielDaten;
+import de.dhpoly.spieler.model.SpielerDaten;
+import de.dhpoly.spieler.model.SpielerTyp;
 
 public class Main
 {
@@ -14,6 +21,14 @@ public class Main
 
 	private void starteServer()
 	{
-		// TODO Auto-generated method stub
+		// TODO Server starten
+
+		List<SpielerDaten> spieler = new ArrayList<>();
+		spieler.add(new SpielerDaten(SpielerTyp.LOKAL, "Rico"));
+		spieler.add(new SpielerDaten(SpielerTyp.LOKAL, "Sven"));
+		spieler.add(new SpielerDaten(SpielerTyp.COMPUTER, "Alex"));
+
+		SpielDaten daten = new SpielDaten(spieler, new Einstellungen());
+		// server.senden(daten);
 	}
 }
