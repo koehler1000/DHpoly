99f1b38d26250b672ce82b02f06883f71579c600
K3 <K3@R-K-07>
2018-05-03 08:22:53 +0200

    Ansicht eingefuegt
    #DHP-166

--- viewtest/de/dhpoly/netzwerk/NetzwerkClientVorschau.java
+++ viewtest/de/dhpoly/netzwerk/NetzwerkClientVorschau.java
@@ -1,7 +1,12 @@
 package de.dhpoly.netzwerk;
 
+import de.dhpoly.bilderverwalter.Bilderverwalter;
 import de.dhpoly.netzwerk.control.NetzwerkClientImpl;
 import de.dhpoly.netzwerk.control.NetzwerkServerImpl;
+import de.dhpoly.oberflaeche.view.Fenster;
+import de.dhpoly.oberflaeche.view.SpielfeldAnsicht;
+import de.dhpoly.spieler.model.SpielerDaten;
+import de.dhpoly.spieler.model.SpielerTyp;
 
 public class NetzwerkClientVorschau
 {
@@ -10,6 +15,13 @@ public class NetzwerkClientVorschau
 		NetzwerkServer server = new NetzwerkServerImpl();
 		NetzwerkClient client = new NetzwerkClientImpl("127.0.0.1");
 		client.connectToServer();
+
+		Fenster fenster = new Fenster(new Bilderverwalter());
+		SpielfeldAnsicht ansicht = new SpielfeldAnsicht(new SpielerDaten(SpielerTyp.NETZWERK, "Netzwerkspieler"),
+				client);
+		fenster.zeigeSpielansicht(ansicht, "NetzwerkClientVorschau");
+
+		client.addAnsicht(ansicht);
 		server.sende("hallo");
 	}
 }
