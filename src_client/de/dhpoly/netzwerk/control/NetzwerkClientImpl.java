7f096273d7933bcb404057f236c33f29246614a8
Sven <Sven@DESKTOP-D3411IO>
2018-05-04 13:57:52 +0200

    Richtiges Package eingefügt
    #DHP-162

--- src_client/de/dhpoly/netzwerk/control/NetzwerkClientImpl.java
+++ src_client/de/dhpoly/netzwerk/control/NetzwerkClientImpl.java
@@ -1,3 +1,5 @@
+package de.dhpoly.netzwerk.control;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
--- src_server/de/dhpoly/netzwerk/control/NetzwerkServerImpl.java
+++ src_server/de/dhpoly/netzwerk/control/NetzwerkServerImpl.java
@@ -1,3 +1,5 @@
+package de.dhpoly.netzwerk.control;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
