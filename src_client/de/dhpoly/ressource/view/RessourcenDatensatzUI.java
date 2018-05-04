7e55810c7d256f2f989cfa21629447075aa9f347
K3 <K3@R-K-07>
2018-05-04 10:28:56 +0200

    SonarLint

--- src_client/de/dhpoly/ressource/view/RessourcenDatensatzUI.java
+++ src_client/de/dhpoly/ressource/view/RessourcenDatensatzUI.java
@@ -3,14 +3,13 @@ package de.dhpoly.ressource.view;
 import de.dhpoly.oberflaeche.view.Oberflaeche;
 import de.dhpoly.oberflaeche.view.SpielfeldAnsicht;
 
-public class RessourcenDatensatzUI extends Oberflaeche
+public class RessourcenDatensatzUI extends Oberflaeche // NOSONAR
 {
 	private static final long serialVersionUID = 1L;
 
 	public RessourcenDatensatzUI(SpielfeldAnsicht ansicht)
 	{
 		super(ansicht);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
