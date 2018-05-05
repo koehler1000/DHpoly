330118989a1f18ae4d0a4e9cc9b33361d9e32608
K3 <K3@R-K-07>
2018-05-06 00:25:12 +0200

    init

---
+++ src_client/de/dhpoly/feld/view/BewegungLogik.java
@@ -0,0 +1,15 @@
+package de.dhpoly.feld.view;
+
+import de.dhpoly.datenobjekt.Datenobjekt;
+import de.dhpoly.logik.Logik;
+import de.dhpoly.spiel.Spiel;
+
+public class BewegungLogik implements Logik
+{
+	@Override
+	public void verarbeite(Datenobjekt objekt, Spiel spiel)
+	{
+		// TODO Auto-generated method stub
+
+	}
+}
