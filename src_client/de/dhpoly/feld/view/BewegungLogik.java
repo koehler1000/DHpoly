03af4bd10a9fec9319c96ade4f730b993fef57de
K3 <K3@R-K-07>
2018-05-06 00:26:10 +0200

    init

--- src_client/de/dhpoly/feld/view/BewegungLogik.java
+++ src_client/de/dhpoly/feld/view/BewegungLogik.java
@@ -1,6 +1,7 @@
 package de.dhpoly.feld.view;
 
 import de.dhpoly.datenobjekt.Datenobjekt;
+import de.dhpoly.feld.model.Bewegung;
 import de.dhpoly.logik.Logik;
 import de.dhpoly.spiel.Spiel;
 
@@ -9,7 +10,10 @@ public class BewegungLogik implements Logik
 	@Override
 	public void verarbeite(Datenobjekt objekt, Spiel spiel)
 	{
-		// TODO Auto-generated method stub
-
+		if (objekt instanceof Bewegung)
+		{
+			Bewegung bewegung = (Bewegung) objekt;
+			// TODO bewegen
+		}
 	}
 }
