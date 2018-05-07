ccc83cdf7c5c416f9242121270f47cd9be273d79
K3 <K3@R-K-07>
2018-05-07 13:29:50 +0200

    Fehler behoben: Tippfehler
    #DHP-175

--- src_server/de/dhpoly/fehler/control/TelegamBenachrichtiger.java
+++ src_server/de/dhpoly/fehler/control/TelegamBenachrichtiger.java
@@ -5,16 +5,15 @@ import java.net.URL;
 import java.net.URLConnection;
 
 import de.dhpoly.datenobjekt.Datenobjekt;
-import de.dhpoly.fehler.FehlerInterface;
 import de.dhpoly.fehler.model.Fehler;
+import de.dhpoly.logik.Logik;
 import de.dhpoly.nachricht.model.Nachricht;
 import de.dhpoly.spiel.Spiel;
 
-public class TelegamBenachrichtiger implements FehlerInterface
+public class TelegamBenachrichtiger implements Logik
 {
 	private static final String CHAT_ID = "-1001131918455";
 
-	@Override
 	public void sendTelegramMessage(String thema, String nachricht) throws IOException
 	{
 		String toSend = thema //
@@ -42,7 +41,7 @@ public class TelegamBenachrichtiger implements FehlerInterface
 				Nachricht nachricht = (Nachricht) objekt;
 				sendTelegramMessage(nachricht.getTitel(), nachricht.getNachricht());
 			}
-			else if (objekt instanceof FehlerInterface)
+			else if (objekt instanceof Fehler)
 			{
 				Fehler fehler = (Fehler) objekt;
 				sendTelegramMessage(fehler.getTitel(), fehler.getFehlertext());
