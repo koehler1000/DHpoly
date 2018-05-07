0bd7703231c59f81e9641b977d1f80a9d6793f7b
K3 <K3@R-K-07>
2018-05-07 15:06:25 +0200

    Wuerfeln wird nun auch aufgerufen (AI)

--- src_ai/de/dhpoly/ai/AI.java
+++ src_ai/de/dhpoly/ai/AI.java
@@ -10,15 +10,17 @@ import de.dhpoly.netzwerk.NetzwerkClient;
 import de.dhpoly.netzwerk.control.NetzwerkClientImpl;
 import de.dhpoly.spieler.model.Spieler;
 import de.dhpoly.spieler.model.SpielerTyp;
+import de.dhpoly.wuerfel.model.WuerfelAufruf;
 
 public class AI implements Datenobjektverwalter
 {
 	NetzwerkClient client;
+	Spieler spieler;
 
 	public void erzeugeComputerspieler(String ip, String name) throws IOException
 	{
 		client = new NetzwerkClientImpl(ip);
-		Spieler spieler = new Spieler(SpielerTyp.COMPUTER, name);
+		spieler = new Spieler(SpielerTyp.COMPUTER, name);
 		client.sendeAnServer(spieler);
 		client.setDatenobjektverwalter(this);
 	}
@@ -31,6 +33,16 @@ public class AI implements Datenobjektverwalter
 			Transaktion transaktion = (Transaktion) datenobjekt;
 			transaktion.setTransaktionsTyp(TransaktionsTyp.ABGELEHNT);
 			client.sendeAnServer(transaktion);
+		}
+
+		else if (datenobjekt instanceof Spieler)
+		{
+			Spieler s = (Spieler) datenobjekt;
+			if (this.spieler == s && s.isAnDerReihe())
+			{
+				WuerfelAufruf aufruf = new WuerfelAufruf(this.spieler);
+				client.sendeAnServer(aufruf);
+			}
 		}
 	}
 }
