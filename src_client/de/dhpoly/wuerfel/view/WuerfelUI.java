197aba5a6f284ba76dfbebd654e8bcb58f670273
K3 <K3@R-K-07>
2018-05-09 01:12:57 +0200

    Anzeigeoptimierung: Wuerfel werden dynamisch erstellt

--- src_client/de/dhpoly/wuerfel/view/WuerfelUI.java
+++ src_client/de/dhpoly/wuerfel/view/WuerfelUI.java
@@ -1,31 +1,41 @@
 package de.dhpoly.wuerfel.view;
 
+import java.awt.GridLayout;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.JLabel;
 
 import de.dhpoly.bilderverwalter.Bilderverwalter;
 import de.dhpoly.oberflaeche.ElementFactory;
 import de.dhpoly.oberflaeche.view.Oberflaeche;
 import de.dhpoly.oberflaeche.view.SpielfeldAnsicht;
+import de.dhpoly.wuerfel.model.Wuerfel;
 import de.dhpoly.wuerfel.model.WuerfelDaten;
 
 public class WuerfelUI extends Oberflaeche // NOSONAR
 {
 	private static final long serialVersionUID = 1L;
 
-	private JLabel lblWuerfelBild = new JLabel();
-
-	private transient WuerfelDaten wuerfel;
-
 	private transient Bilderverwalter bilderverwalter = new Bilderverwalter();
 
 	public WuerfelUI(WuerfelDaten wuerfel, SpielfeldAnsicht ansicht)
 	{
 		super(ansicht);
-		this.wuerfel = wuerfel;
 
 		ElementFactory.bearbeitePanel(this);
-		this.add(lblWuerfelBild);
-		lblWuerfelBild.setIcon(bilderverwalter.getWuerfelBild(wuerfel.getWuerfel().get(0).getZahl()));
+		this.setLayout(new GridLayout(1, 1));
+
+		List<JLabel> lblsWuerfel = new ArrayList<>();
+
+		for (Wuerfel w : wuerfel.getWuerfel())
+		{
+			JLabel lblW = new JLabel();
+			lblW.setIcon(bilderverwalter.getWuerfelBild(w.getZahl()));
+			lblsWuerfel.add(lblW);
+		}
+
+		lblsWuerfel.forEach(this::add);
 	}
 
 	@Override
