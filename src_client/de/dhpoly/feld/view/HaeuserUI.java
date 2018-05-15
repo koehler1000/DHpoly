1eaf7ebb6233e349cbac5c9bea24e76885862e49
K3 <K3@R-K-07>
2018-05-15 16:31:06 +0200

    Refactoring: Gleiches Design

--- src_client/de/dhpoly/feld/view/HaeuserUI.java
+++ src_client/de/dhpoly/feld/view/HaeuserUI.java
@@ -3,7 +3,10 @@ package de.dhpoly.feld.view;
 import java.awt.GridLayout;
 import java.util.List;
 
+import javax.swing.JPanel;
+
 import de.dhpoly.feld.model.StrasseDaten;
+import de.dhpoly.oberflaeche.ElementFactory;
 import de.dhpoly.oberflaeche.view.Oberflaeche;
 import de.dhpoly.oberflaeche.view.SpielfeldAnsicht;
 
@@ -14,12 +17,16 @@ public class HaeuserUI extends Oberflaeche // NOSONAR
 	public HaeuserUI(List<StrasseDaten> felder, SpielfeldAnsicht ansicht)
 	{
 		super(ansicht);
-		this.setLayout(new GridLayout(felder.size(), 1, 10, 10));
+
+		JPanel pnlInhalt = ElementFactory.erzeugePanel();
+		pnlInhalt.setLayout(new GridLayout(felder.size(), 1, 10, 10));
 
 		for (StrasseDaten strasse : felder)
 		{
-			this.add(new HausUI(strasse, ansicht));
+			pnlInhalt.add(new HausUI(strasse, ansicht));
 		}
+
+		this.add(pnlInhalt);
 	}
 
 	@Override
