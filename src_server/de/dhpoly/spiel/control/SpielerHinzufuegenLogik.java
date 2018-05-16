a028679a8e5fa0113810d640356dfe4b37f23cb6
K3 <K3@R-K-07>
2018-05-16 10:20:16 +0200

    SpielerHinzufuegenLogik implementiert

---
+++ src_server/de/dhpoly/spiel/control/SpielerHinzufuegenLogik.java
@@ -0,0 +1,20 @@
+package de.dhpoly.spiel.control;
+
+import de.dhpoly.datenobjekt.Datenobjekt;
+import de.dhpoly.logik.Logik;
+import de.dhpoly.spiel.Spiel;
+import de.dhpoly.spieler.model.Spieler;
+
+public class SpielerHinzufuegenLogik implements Logik
+{
+
+	@Override
+	public void verarbeite(Datenobjekt objekt, Spiel spiel)
+	{
+		if (objekt instanceof Spieler)
+		{
+			spiel.fuegeSpielerHinzu((Spieler) objekt);
+		}
+	}
+
+}
