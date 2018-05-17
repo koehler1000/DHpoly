13636c13e64e05b637256304d5fbbd3201f53f63
K3 <K3@R-K-07>
2018-05-17 08:37:46 +0200

    Refactoring

--- test/de/dhpoly/netzwerk/control/NetzwerkImplTest.java
+++ test/de/dhpoly/netzwerk/control/NetzwerkImplTest.java
@@ -1,23 +1,15 @@
 package de.dhpoly.netzwerk.control;
 
-import static org.junit.Assert.*;
-
 import java.io.IOException;
 
 import org.junit.Test;
 
-public class NetzwerkImplTest {
-
-	@Test
-	public void ClientTest() {
+public class NetzwerkImplTest
+{
+	@Test(expected = IOException.class)
+	public void ClientTest() throws IOException
+	{
 		NetzwerkClientImpl client = new NetzwerkClientImpl("Test");
-		try {
-			client.verbinden("", 1234);
-			fail("Not yet implemented");
-		} catch (IOException e) {
-			assertTrue(true);
-			e.printStackTrace();
-		}
+		client.verbinden("", 1234);
 	}
-
 }
