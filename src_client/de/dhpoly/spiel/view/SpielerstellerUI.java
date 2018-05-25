a3f89b9b40f6fa0c0a50017ecb4d095903f776c4
K3 <K3@R-K-07>
2018-05-25 09:32:08 +0200

    Refactoring: Auslagerung in mehrere Klassen

---
+++ src_client/de/dhpoly/spiel/view/SpielerstellerUI.java
@@ -0,0 +1,47 @@
+package de.dhpoly.spiel.view;
+
+import java.awt.Component;
+
+import javax.swing.JButton;
+
+import de.dhpoly.ai.AI;
+import de.dhpoly.netzwerk.NetzwerkClient;
+import de.dhpoly.netzwerk.NetzwerkServer;
+import de.dhpoly.oberflaeche.ElementFactory;
+import de.dhpoly.oberflaeche.view.Fenster;
+import de.dhpoly.oberflaeche.view.SpielUI;
+import de.dhpoly.spiel.control.SpielImpl;
+import de.dhpoly.spieler.model.Spieler;
+
+public class SpielerstellerUI
+{
+	private Fenster fenster;
+	private NetzwerkServer server;
+	private NetzwerkClient client;
+
+	public SpielerstellerUI(Fenster fenster, NetzwerkServer server, NetzwerkClient client)
+	{
+		this.fenster = fenster;
+		this.server = server;
+		this.client = client;
+		JButton butSpielHosten = ElementFactory.getButtonUeberschrift("Spiel hosten");
+		butSpielHosten.addActionListener(e -> spielHosten(butSpielHosten));
+		fenster.zeigeComponente(butSpielHosten, "+");
+	}
+
+	private void spielHosten(Component c)
+	{
+		fenster.loescheKomponente(c);
+		new SpielImpl(server);
+		spielerHinzu("Hans");
+		new AI().erzeugeComputerspieler(client, "PC1");
+	}
+
+	private void spielerHinzu(String name)
+	{
+		Spieler spieler = new Spieler(name);
+		SpielUI verwalter2 = new SpielUI(spieler, client);
+		fenster.zeigeSpielansicht(verwalter2, name);
+		verwalter2.sendeAnServer(spieler);
+	}
+}
--- src_main/de/dhpoly/main/MainFake.java
+++ src_main/de/dhpoly/main/MainFake.java
@@ -1,26 +1,15 @@
 package de.dhpoly.main;
 
-import java.awt.Component;
 import java.io.IOException;
 
-import javax.swing.JButton;
-
-import de.dhpoly.ai.AI;
 import de.dhpoly.bilderverwalter.Bilderverwalter;
 import de.dhpoly.fakes.ClientFake;
 import de.dhpoly.fakes.ServerFake;
-import de.dhpoly.oberflaeche.ElementFactory;
 import de.dhpoly.oberflaeche.view.Fenster;
-import de.dhpoly.oberflaeche.view.SpielUI;
-import de.dhpoly.spiel.control.SpielImpl;
-import de.dhpoly.spieler.model.Spieler;
+import de.dhpoly.spiel.view.SpielerstellerUI;
 
 public class MainFake
 {
-	private Fenster fenster = new Fenster(new Bilderverwalter());
-	private ClientFake client = ClientFake.CLIENT_FAKE;
-	private ServerFake server = ServerFake.SERVER_FAKE;
-
 	public static void main(String[] args) throws IOException
 	{
 		new MainFake();
@@ -28,24 +17,9 @@ public class MainFake
 
 	public MainFake() throws IOException
 	{
-		JButton butSpielHosten = ElementFactory.getButtonUeberschrift("Spiel hosten");
-		butSpielHosten.addActionListener(e -> spielHosten(butSpielHosten));
-		fenster.zeigeComponente(butSpielHosten, "+");
-	}
-
-	private void spielHosten(Component c)
-	{
-		fenster.loescheKomponente(c);
-		new SpielImpl(server);
-		spielerHinzu("Hans");
-		new AI().erzeugeComputerspieler(client, "PC1");
-	}
-
-	private void spielerHinzu(String name)
-	{
-		Spieler spieler = new Spieler(name);
-		SpielUI verwalter2 = new SpielUI(spieler, client);
-		fenster.zeigeSpielansicht(verwalter2, name);
-		verwalter2.sendeAnServer(spieler);
+		Fenster fenster = new Fenster(new Bilderverwalter());
+		ClientFake client = ClientFake.CLIENT_FAKE;
+		ServerFake server = ServerFake.SERVER_FAKE;
+		new SpielerstellerUI(fenster, server, client);
 	}
 }
