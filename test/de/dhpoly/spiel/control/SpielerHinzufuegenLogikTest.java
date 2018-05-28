66067e4b001cabd27430d067fb5bf27de6df8076
K3 <K3@R-K-07>
2018-05-28 10:43:57 +0200

    Test hinzugefuegt

---
+++ test/de/dhpoly/spiel/control/SpielerHinzufuegenLogikTest.java
@@ -0,0 +1,38 @@
+package de.dhpoly.spiel.control;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import de.dhpoly.spiel.model.SpielStatus;
+import de.dhpoly.spieler.model.Spieler;
+
+public class SpielerHinzufuegenLogikTest
+{
+	private SpielerHinzufuegenLogik logik = new SpielerHinzufuegenLogik();
+
+	@Test
+	public void spielWirdGestartet() throws Exception
+	{
+		Spieler spieler = new Spieler("bla");
+		logik.verarbeite(spieler, spiel);
+
+		assertTrue(spielerHinzugefuegt);
+	}
+
+	private boolean spielerHinzugefuegt = false;
+	private SpielUnimplemented spiel = new SpielUnimplemented()
+	{
+		@Override
+		public void fuegeSpielerHinzu(Spieler spieler)
+		{
+			spielerHinzugefuegt = true;
+		}
+
+		@Override
+		public SpielStatus getStatus()
+		{
+			return SpielStatus.SPIEL_VORBEREITUNG;
+		}
+	};
+}
