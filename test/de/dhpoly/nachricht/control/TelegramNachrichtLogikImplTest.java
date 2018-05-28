874acb547977ccb329611e832df1886c4f01f305
K3 <K3@R-K-07>
2018-05-28 10:51:31 +0200

    Refactoring: Umbenannt

--- test/de/dhpoly/nachricht/control/TelegramNachrichtLogikImplTest.java
+++ test/de/dhpoly/nachricht/control/TelegramNachrichtLogikImplTest.java
@@ -1,6 +1,27 @@
 package de.dhpoly.nachricht.control;
 
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
 public class TelegramNachrichtLogikImplTest
 {
-
+	@Test
+	@Ignore
+	public void test() // NOSONAR
+	{
+		try
+		{
+			new TelegramNachrichtLogikImpl().sendTelegramMessage("Testfehler",
+					"Das ist ein Fehler aus dem JUnitTest. Dieser ist beabsichtigt und darf ignoriert werden.");
+		}
+		catch (IOException ex)
+		{
+			System.err.println("Fehler beim Senden der Nachricht - " + ex.getClass() + " - " + ex.getMessage());
+			fail("Senden fehlgeschlagen - " + ex.getMessage());
+		}
+	}
 }
