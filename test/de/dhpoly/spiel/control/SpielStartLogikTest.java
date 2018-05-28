dd3496413f955a0441d0833d439a1e03d11f715f
K3 <K3@R-K-07>
2018-05-28 10:40:43 +0200

    Test fuer SpielStart

---
+++ test/de/dhpoly/spiel/control/SpielStartLogikTest.java
@@ -0,0 +1,39 @@
+package de.dhpoly.spiel.control;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import de.dhpoly.spiel.model.SpielStart;
+import de.dhpoly.spiel.model.SpielStatus;
+import de.dhpoly.spieler.model.Spieler;
+
+public class SpielStartLogikTest
+{
+	private SpielStartLogik logik = new SpielStartLogik();
+
+	@Test
+	public void spielWirdGestartet() throws Exception
+	{
+		SpielStart start = new SpielStart(new Spieler(""));
+		logik.verarbeite(start, spiel);
+
+		assertTrue(spielStartAufgerufen);
+	}
+
+	private boolean spielStartAufgerufen = false;
+	private SpielUnimplemented spiel = new SpielUnimplemented()
+	{
+		@Override
+		public void starteSpiel()
+		{
+			spielStartAufgerufen = true;
+		}
+
+		@Override
+		public SpielStatus getStatus()
+		{
+			return SpielStatus.SPIEL_VORBEREITUNG;
+		}
+	};
+}
