56ae20232c580150338c814108eac206e6f900e0
alexb <alexb@DESKTOP-D20HACA>
2018-06-11 14:16:05 +0200

    Nachricht, wenn Spieler hinzugefügt wird

--- src/server/de/dhpoly/spiel/control/SpielerHinzufuegenLogik.java
+++ src/server/de/dhpoly/spiel/control/SpielerHinzufuegenLogik.java
@@ -1,7 +1,9 @@
 package de.dhpoly.spiel.control;
 
 import de.dhpoly.datenobjekt.Datenobjekt;
+import de.dhpoly.empfaenger.model.Empfaenger;
 import de.dhpoly.logik.Logik;
+import de.dhpoly.nachricht.model.Nachricht;
 import de.dhpoly.spiel.Spiel;
 import de.dhpoly.spieler.model.Spieler;
 
@@ -14,6 +16,8 @@ public class SpielerHinzufuegenLogik implements Logik
 		if (objekt instanceof Spieler)
 		{
 			spiel.fuegeSpielerHinzu((Spieler) objekt);
+			Nachricht nachricht = new Nachricht("Aktuelle Spieleranzahl: " + spiel.getSpieler().size(), Empfaenger.ALLE_SPIELER);
+			spiel.empfange(nachricht);
 		}
 	}
 
