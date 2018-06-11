d96f673cd0cb51e42d17bdbcc4d3e754f4cc43eb
koehler1000 <koehler9@gmail.com>
2018-06-11 10:22:42 +0200

    Deserialisierung
    #DHP-192

--- src/netzwerk/de/dhpoly/netzwerk/control/Serialisierer.java
+++ src/netzwerk/de/dhpoly/netzwerk/control/Serialisierer.java
@@ -1,7 +1,10 @@
 package de.dhpoly.netzwerk.control;
 
 import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.Base64;
@@ -18,5 +21,14 @@ public class Serialisierer
 		oos.writeObject(o);
 		oos.close();
 		return Base64.getEncoder().encodeToString(baos.toByteArray());
+	}
+	
+	public static Serializable toObject(String s) throws IOException, ClassNotFoundException {
+		FileInputStream fileIn = new FileInputStream(s);
+		ObjectInputStream in = new ObjectInputStream(fileIn);
+		Serializable s1 = (Serializable) in.readObject();
+		in.close();
+		fileIn.close();
+		return s1;
 	}
 }
