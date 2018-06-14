6b4d48bf290b473c366f911296a7e7aaa03164ae
K3 <K3@R-K-07>
2018-06-14 15:49:35 +0200

    Fehler behoben

--- src/test/de/dhpoly/spiel/control/SpielStartLogikTest.java
+++ src/test/de/dhpoly/spiel/control/SpielStartLogikTest.java
@@ -2,6 +2,9 @@ package de.dhpoly.spiel.control;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 import de.dhpoly.spiel.model.SpielStart;
@@ -34,6 +37,13 @@ public class SpielStartLogikTest
 		public SpielStatus getStatus()
 		{
 			return SpielStatus.SPIEL_VORBEREITUNG;
+		}
+
+		public List<Spieler> getSpieler()
+		{
+			List<Spieler> spieler = new ArrayList<>();
+			spieler.add(new Spieler("bla"));
+			return spieler;
 		}
 	};
 }
