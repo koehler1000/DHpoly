4678dcfba7f0aadb3bf0eb1ffbbbfb026c792d06
K3 <K3@R-K-07>
2018-06-14 13:33:38 +0200

    Spieler hinzufuegen erzeugt nun keine Nachrichten mehr

--- src/server/de/dhpoly/spiel/control/SpielerHinzufuegenLogik.java
+++ src/server/de/dhpoly/spiel/control/SpielerHinzufuegenLogik.java
@@ -1,9 +1,7 @@
 package de.dhpoly.spiel.control;
 
 import de.dhpoly.datenobjekt.Datenobjekt;
-import de.dhpoly.empfaenger.model.Empfaenger;
 import de.dhpoly.logik.Logik;
-import de.dhpoly.nachricht.model.Nachricht;
 import de.dhpoly.spiel.Spiel;
 import de.dhpoly.spieler.model.Spieler;
 
@@ -16,8 +14,6 @@ public class SpielerHinzufuegenLogik implements Logik
 		if (objekt instanceof Spieler)
 		{
 			spiel.fuegeSpielerHinzu((Spieler) objekt);
-			Nachricht nachricht = new Nachricht("Aktuelle Spieleranzahl: " + spiel.getSpieler().size(), Empfaenger.ALLE_SPIELER);
-			spiel.empfange(nachricht);
 		}
 	}
 
